ba98d8ffc0796f41438e0304948a600a68eddfad
chuhong <chuhongwang@outlook.com>
2024-01-31 23:08:30 -0500

    cleanup

--- src/kernel.cu
+++ src/kernel.cu
@@ -384,123 +384,6 @@ __global__ void kernIdentifyCellStartEnd(int N, int *particleGridIndices,
   }
 }
 
-__global__ void kernUpdateVelNeighborSearchScattered_ref(
-    int N, int gridResolution, glm::vec3 gridMin,
-    float inverseCellWidth, float cellWidth,
-    int *gridCellStartIndices, int *gridCellEndIndices,
-    int *particleArrayIndices,
-    glm::vec3 *pos, glm::vec3 *vel1, glm::vec3 *vel2) {
-    // DONE-2.1 - Update a boid's velocity using the uniform grid to reduce
-    // the number of boids that need to be checked.
-    // - Identify the grid cell that this particle is in
-    // - Identify which cells may contain neighbors. This isn't always 8.
-    // - For each cell, read the start/end indices in the boid pointer array.
-    // - Access each boid in the cell and compute velocity change from
-    //   the boids rules, if this boid is within the neighborhood distance.
-    // - Clamp the speed change before putting the new speed in vel2
-
-    int index = threadIdx.x + (blockIdx.x * blockDim.x);
-    if (index >= N)
-    {
-        return;
-    }
-
-    glm::vec3 pos_self = pos[index];
-    glm::vec3 grid_cell = inverseCellWidth * (pos_self - gridMin);
-    glm::vec3 grid_cell_int = glm::floor(grid_cell);
-    glm::vec3 grid_cell_frac = grid_cell - grid_cell_int;
-
-    // The following vectors store whether neighbors in the positive and negative direction should be checked
-    // glm::ivec3 check_neg;
-    // glm::ivec3 check_pos;
-
-    // check_neg.x = (grid_cell_frac.x <= 0.5f && grid_cell_int.x > 0) ? 1.f : 0.f;
-    // check_neg.y = (grid_cell_frac.y <= 0.5f && grid_cell_int.y > 0) ? 1.f : 0.f;
-    // check_neg.z = (grid_cell_frac.z <= 0.5f && grid_cell_int.z > 0) ? 1.f : 0.f;
-    // check_pos.x = (grid_cell_frac.x > 0.5f  && grid_cell_int.x < gridResolution - 1) ? 1.f : 0.f;
-    // check_pos.y = (grid_cell_frac.y > 0.5f  && grid_cell_int.y < gridResolution - 1) ? 1.f : 0.f;
-    // check_pos.z = (grid_cell_frac.z > 0.5f  && grid_cell_int.z < gridResolution - 1) ? 1.f : 0.f;
-
-    // Velocity change due to each rule
-    // Boids try to fly towards center of mass of neighboring boids
-    glm::vec3 velocity_change;
-    glm::vec3 perceived_center;
-    glm::vec3 c;
-    glm::vec3 perceived_velocity;
-
-    int num_neighbors_r1 = 0;
-    int num_neighbors_r3 = 0;
-
-    // for (int z = grid_cell_int.z - check_neg.z; z <= grid_cell_int.z + check_pos.z; z++)
-    // {
-    //     for (int y = grid_cell_int.y - check_neg.y; y <= grid_cell_int.y + check_pos.y; y++)
-    //     {
-    //         for (int x = grid_cell_int.x - check_neg.x; x <= grid_cell_int.x + check_pos.x; x++)
-    //         {
-                int x = grid_cell_int.x, y= grid_cell_int.y, z = grid_cell_int.z; 
-                int neighbor_grid_cell_1D = gridIndex3Dto1D(x, y, z, gridResolution);
-
-                if (gridCellStartIndices[neighbor_grid_cell_1D] == -1)
-                {
-                    return;
-                }
-
-                for (int cell_index = gridCellStartIndices[neighbor_grid_cell_1D]; cell_index <= gridCellEndIndices[neighbor_grid_cell_1D]; cell_index++)
-                {
-                    int index_other = particleArrayIndices[cell_index];
-                    glm::vec3 pos_other = pos[index_other];
-
-                    if (index_other != index) {
-                        float dist_to_other = glm::distance(pos_other, pos_self);
-
-                        // Rule 1: boids fly towards their local perceived center of mass, which excludes themselves
-                        if (dist_to_other < rule1Distance)
-                        {
-                            perceived_center += pos_other;
-                            num_neighbors_r1++;
-                        }
-
-                        // Rule 2: boids try to stay a distance d away from each other
-                        if (dist_to_other < rule2Distance)
-                        {
-                            c -= (pos_other - pos_self);
-                        }
-
-                        // Rule 3: boids try to match the speed of surrounding boids
-                        if (dist_to_other < rule3Distance) {
-                            perceived_velocity += vel1[index_other];
-                            num_neighbors_r3++;
-                        }
-                    }
-                }
-    //         }
-    //     }
-    // }
-
-    // Calculate contributions for each rule
-    // Rule 1
-    if (num_neighbors_r1 > 0)
-    {
-        velocity_change += (perceived_center / (float)num_neighbors_r1 - pos_self) * rule1Scale;
-    }
-
-    // Rule 2
-    velocity_change += c * rule2Scale;
-
-    // Rule 3
-    if (num_neighbors_r3 > 0)
-    {
-        velocity_change += (perceived_velocity / (float)num_neighbors_r3) * rule3Scale;
-    }
-
-    // - Clamp the speed change before putting the new speed in vel2
-    glm::vec3 new_velocity = vel1[index] + velocity_change;
-    if (glm::length(new_velocity) > maxSpeed)
-    {
-        new_velocity = maxSpeed * glm::normalize(new_velocity);
-    }
-    vel2[index] = new_velocity;
-}
 __global__ void kernUpdateVelNeighborSearchScattered(
   int N, int gridResolution, glm::vec3 gridMin,
   float inverseCellWidth, float cellWidth,
@@ -603,28 +486,7 @@ __global__ void kernUpdateVelNeighborSearchScattered(
 
     glm::vec3 new_velo = vel1[index] + velocity_change; 
     vel2[index] = glm::length(new_velo)>maxSpeed? maxSpeed*glm::normalize(new_velo):new_velo; 
-    // if (num_neighbors1 > 0)
-    // {
-    //     velocity_change += (perceived_center / (float)num_neighbors1 - curr_pos) * rule1Scale;
-    // }
-
-    // // Rule 2
-    // velocity_change += c * rule2Scale;
-
-    // // Rule 3
-    // if (num_neighbors3 > 0)
-    // {
-    //     velocity_change += (perceived_velocity / (float)num_neighbors3) * rule3Scale;
-    // }
-
-    // // - Clamp the speed change before putting the new speed in vel2
-    // glm::vec3 new_velocity = vel1[index] + velocity_change;
-    // if (glm::length(new_velocity) > maxSpeed)
-    // {
-    //     new_velocity = maxSpeed * glm::normalize(new_velocity);
-    // }
-    // vel2[index] = new_velocity;
-    } 
+  }
   }
 
 
@@ -663,8 +525,8 @@ void Boids::stepSimulationNaive(float dt) {
   glm::vec3* temp = dev_vel2;
   dev_vel2 = dev_vel1;
   dev_vel1 = temp;
-
 }
+
 
 void Boids::stepSimulationScatteredGrid(float dt) {
   // TODO-2.1
